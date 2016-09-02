06af738abc3a7e6d5b2f3e497e0ee0531b5d8dce
Rishi Patel <rishi@docnavlab.com>
2016-09-01 23:31:39 -0400

    fix clearcontents_ws()

--- README.md
+++ README.md
@@ -9,5 +9,8 @@ Include code below in file that need to use xlwings-utilities
 ```py
 import os, sys
 sys.path.append(os.path.abspath('/Users/rishi/git/xlwings-utilities/')) # This should be the path to the local repo
-from xlwingswrapper import clear, clearcontents, list_write, list_read, df_read, df_write
+from xlwings import Workbook, Sheet, Range
+from xlwings_utilities import clear, clearcontents, list_write, list_read, df_read, df_write
+# Create new workbook
+wb = Workbook()
 ```
--- xlwings_utilities.py
+++ xlwings_utilities.py
@@ -1,59 +1,78 @@
 """
-To use:
 
-import os, sys
-sys.path.append(os.path.abspath('/Users/rishi/git/xlwings-utilities/'))
-from xlwingswrapper import clear, clearcontents, list_write, list_read, df_read, df_write
+Wrapper functions for xlwings
 
 """
-
-# Wrapper functions for xlwings
 
 import pandas as pd
 from xlwings import Workbook, Sheet, Range
 
 # Utililty functions for dataframes
 def clear(range_name, ws=None):
-    """Clear a table started in a range cell, ex: clear('a1')"""
+    """Clear a table started in a range cell, ex: clear('a1').
+
+    clear(range_name, ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
     Range(ws.name, range_name).table.clear()
 
 def clear_ws(ws=None):
-    """Clear a table started in a range cell, ex: clearcontents('a1')"""
+    """Clear a table started in a range cell, ex: clearcontents('a1').
+
+    clear_ws(ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
-    Range(ws.name).clear()
+    ws.clear()
 
 def clearcontents(range_name, ws=None):
-    """Clear a table started in a range cell, ex: clearcontents('a1')"""
+    """Clear a table started in a range cell, ex: clearcontents('a1').
+
+    clearcontents(range_name, ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
     Range(ws.name, range_name).table.clear_contents()
 
 def clearcontents_ws(ws=None):
-    """Clear a table started in a range cell, ex: clearcontents('a1')"""
+    """Clear entire worksheet, ex: clearcontents_ws(ws=worksheetobj).
+
+    clearcontents_ws(ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
-    Range(ws.name).clear_contents()
+    ws.clear_contents()
 
 def list_write(range_name, value_list, ws=None):
-    """Write a list vertially"""
+    """Write a list vertially.
+
+    list_write(range_name, value_list, ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
     # Turn list into a column
     value_column = [[e] for e in value_list]
     Range(ws.name, range_name).value = value_column
 
 def list_read(range_name, value_list, ws=None):
-    """Read a list vertially"""
+    """Read a list vertially.
+
+    list_read(range_name, value_list, ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
     # Range(range_name).options(transpose=True).value = value_list
     datalist = Range(ws.name, range_name).vertical.value
 
 def df_read(range_name, ws=None):
-    """Return dataframe from range name and Sheet.; ex: df = df_read('a1')"""
+    """Return dataframe from range name and Sheet.; ex: df = df_read('a1').
+
+    df_read(range_name, ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
     data = Range(ws.name, range_name).table.value
     df = pd.DataFrame(data[1:], columns=data[0])
     return df
 
-def df_write(range_name, df, ws=None):
-    """Write a dataframe to a cell."""
+def df_write(df, range_name, ws=None):
+    """Write a dataframe to a cell.
+
+    df_write(df, range_name, ws=None)
+    """
     ws = Sheet.active() if ws is None else ws
     Range(ws.name, range_name, index=False).value = df # without pd indices
